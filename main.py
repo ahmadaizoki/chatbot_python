136445159aa20e615eb438d9149ec38891656f9d
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-07-11 15:39:15 +0200

    python bot

---
+++ main.py
@@ -0,0 +1,10 @@
+import Tensorflow_chat_bot_response as bt
+
+def main():
+    bt.response('saluttt toi')
+    bt.response('comment tu vas ?')
+    bt.response('je souhaite accueillir un nouveau consultant')
+    bt.response('je souhaite accueillir consultant')
+
+if __name__=='__main__':
+    main()
